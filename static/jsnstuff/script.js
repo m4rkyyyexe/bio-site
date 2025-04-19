95896a8d0d4c32cd23c14784beb82cd5f620b37f
6thnight <168860117+bunnyhopenjoyer@users.noreply.github.com>
2025-04-19 17:18:19 +0100

    Update script.js

--- static/jsnstuff/script.js
+++ static/jsnstuff/script.js
@@ -36,3 +36,21 @@ function animateTitle(title) {
 }
 
 animateTitle("@6thnight");
+
+jQuery(document).ready(function () {
+    var mouseX = 0, mouseY = 0;
+    var circleWidth = $('.circle').width();
+    var circleHeight = $('.circle').height();
+    var xp = 0, yp = 0;
+
+    $(document).mousemove(function (e) {
+        mouseX = e.pageX - (circleWidth / 2);
+        mouseY = e.pageY - (circleHeight / 2);
+    });
+
+    setInterval(function () {
+        xp += ((mouseX - xp) / 6);
+        yp += ((mouseY - yp) / 6);
+        $("#circle").css({ left: xp + 'px', top: yp + 'px' });
+    }, 17);
+});
